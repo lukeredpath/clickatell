f148c0afdc25a7ef3fe608577e1cdb9373dfc61d
luke <luke@99183692-6b28-0410-aa2f-9dcdcacf4b0b>
2007-08-20 11:56:03 +0000

    Rewrote sms bin file
    git-svn-id: svn://lukeredpath.co.uk/var/svn/opensource/clickatell/trunk@71 99183692-6b28-0410-aa2f-9dcdcacf4b0b

---
+++ lib/clickatell/utility/options.rb
@@ -0,0 +1,48 @@
+require 'optparse'
+require 'ostruct'
+
+module Clickatell
+  module Utility
+    class Options
+      def self.parse(args)
+        options = self.default_options
+        parser = OptionParser.new do |opts|
+          opts.banner = "Usage: sms [options] recipient_number message"
+          opts.separator ""
+          opts.separator "Specific options:"
+          
+          opts.on('-u', '--username USERNAME',
+            "Specify the clickatell username (overrides ~/.clickatell setting)") do |username|
+             options.username = username 
+          end
+          
+          opts.on('-p', '--password PASSWORD',
+            "Specify the clickatell password (overrides ~/.clickatell setting)") do |password|
+             options.password = password
+          end
+          
+          opts.on('-k', '--apikey API_KEY',
+            "Specify the clickatell API key (overrides ~/.clickatell setting)") do |key|
+             options.api_key = key
+          end
+        end
+        parser.parse!(args)
+        options.recipient = ARGV[-2]
+        options.message   = ARGV[-1]
+        return options
+      end
+      
+      def self.default_options
+        options = OpenStruct.new
+        config_file = File.open(File.join(ENV['HOME'], '.clickatell'))
+        config = YAML.load(config_file)
+        options.username = config['username']
+        options.password = config['password']
+        options.api_key  = config['api_key']
+        return options
+      rescue Errno::ENOENT
+        return options
+      end
+    end
+  end
+end
