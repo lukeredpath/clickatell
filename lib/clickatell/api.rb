edcadef755a5b450d97d696aee580821b7c6e976
luke <luke@99183692-6b28-0410-aa2f-9dcdcacf4b0b>
2007-08-23 09:30:14 +0000

    When using utility to check message status, it should display the proper human message instead of just the message status code
    git-svn-id: svn://lukeredpath.co.uk/var/svn/opensource/clickatell/trunk@104 99183692-6b28-0410-aa2f-9dcdcacf4b0b

--- lib/clickatell/api.rb
+++ lib/clickatell/api.rb
@@ -132,5 +132,26 @@ module Clickatell
       end
     end
     
+    class MessageStatus
+      STATUS_MAP = {
+        1  => 'Message unknown',
+        2  => 'Message queued',
+        3  => 'Delivered to gateway',
+        4  => 'Received by recipient',
+        5  => 'Error with message',
+        6  => 'User cancelled messaged delivery',
+        7  => 'Error delivering message',
+        8  => 'OK',
+        9  => 'Routing error',
+        10 => 'Message expired',
+        11 => 'Message queued for later delivery',
+        12 => 'Out of credit'
+      }
+      
+      def self.[](code)
+        STATUS_MAP[code]
+      end
+    end
+    
   end
 end
