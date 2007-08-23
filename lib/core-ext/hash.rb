cb21adc7b3fa38b0a9f3eb8c8a38ab161d911fa2
luke <luke@99183692-6b28-0410-aa2f-9dcdcacf4b0b>
2007-08-23 18:46:17 +0000

    Superfluous self. call
    git-svn-id: svn://lukeredpath.co.uk/var/svn/opensource/clickatell/trunk@110 99183692-6b28-0410-aa2f-9dcdcacf4b0b

--- lib/core-ext/hash.rb
+++ lib/core-ext/hash.rb
@@ -7,7 +7,7 @@ class Hash
   #
   # Keys that do not exist in the original hash are ignored.
   def only(*keys)
-    self.inject({}) do |new_hash, (key, value)|
+    inject( {} ) do |new_hash, (key, value)|
       new_hash[key] = value if keys.include?(key)
       new_hash
     end
