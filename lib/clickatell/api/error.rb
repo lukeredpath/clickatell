bd05825403c9c617dc77e19ace0224616e7f16d0
Lars Pind <lars@pinds.com>
2010-10-01 11:44:42 +0200

    If you're sending multiple SMS'es, don't raise an exception if there's just one problem, give me the whole response back so I can parse it myself

--- lib/clickatell/api/error.rb
+++ lib/clickatell/api/error.rb
@@ -3,10 +3,10 @@ module Clickatell
   
     # Clickatell API Error exception.
     class Error < StandardError
-      attr_reader :code, :message, :response
+      attr_reader :code, :message
       
-      def initialize(code, message, response)
-        @code, @message, @response = code, message, response
+      def initialize(code, message)
+        @code, @message = code, message
       end
       
       # Creates a new Error from a Clickatell HTTP response string
@@ -14,14 +14,12 @@ module Clickatell
       #
       #  Error.parse("ERR: 001, Authentication error")
       #  # =>  #<Clickatell::API::Error code='001' message='Authentication error'>
-      def self.parse(response)
-        response.body.split("\n").map do |line|
-          if line =~ /^ERR: (\d+), (.*)$/
-            code, message = $1.to_i, $2
-            break
-          end
+      def self.parse(error_string)
+        if error_string =~ /^ERR: (\d+), (.*)$/
+          self.new($1, $2)
+        else
+          self.new(nil, error_string)
         end
-        self.new(code, message, response)
       end
     end
   
