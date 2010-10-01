98322b82a10a200806a531fad6987af17c5abf7a
Lars Pind <lars@pinds.com>
2010-10-01 11:34:26 +0200

    Parse response more intelligently, and store the entire response

--- lib/clickatell/api/error.rb
+++ lib/clickatell/api/error.rb
@@ -3,10 +3,10 @@ module Clickatell
   
     # Clickatell API Error exception.
     class Error < StandardError
-      attr_reader :code, :message
+      attr_reader :code, :message, :response
       
-      def initialize(code, message)
-        @code, @message = code, message
+      def initialize(code, message, response)
+        @code, @message, @response = code, message, response
       end
       
       # Creates a new Error from a Clickatell HTTP response string
@@ -14,10 +14,14 @@ module Clickatell
       #
       #  Error.parse("ERR: 001, Authentication error")
       #  # =>  #<Clickatell::API::Error code='001' message='Authentication error'>
-      def self.parse(error_string)
-        error_details = error_string.split(':').last.strip
-        code, message = error_details.split(',').map { |s| s.strip }
-        self.new(code, message)
+      def self.parse(response)
+        response.body.split("\n").map do |line|
+          if line =~ /^ERR: (\d+), (.*)$/
+            code, message = $1.to_i, $2
+            break
+          end
+        end
+        self.new(code, message, response)
       end
     end
   
