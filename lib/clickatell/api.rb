857759171269fefcd999e3e794e6234966130b93
Luke Redpath <contact@lukeredpath.co.uk>
2010-10-12 12:06:33 +0100

    Fixed documentation

--- lib/clickatell/api.rb
+++ lib/clickatell/api.rb
@@ -3,10 +3,10 @@ module Clickatell
   # HTTP service.
   class API
     attr_accessor :auth_options
-    
+
     class << self
-      # Authenticates using the given credentials and returns an 
-      # API instance with the authentication options set to use the 
+      # Authenticates using the given credentials and returns an
+      # API instance with the authentication options set to use the
       # resulting session_id.
       def authenticate(api_id, username, password)
         api = self.new
@@ -14,10 +14,10 @@ module Clickatell
         api.auth_options = { :session_id => session_id }
         api
       end
-      
+
       # Set to true to enable debugging (off by default)
       attr_accessor :debug_mode
-      
+
       # Enable secure mode (SSL)
       attr_accessor :secure_mode
 
@@ -27,25 +27,25 @@ module Clickatell
       # Set to your HTTP proxy details (off by default)
       attr_accessor :proxy_host, :proxy_port, :proxy_username, :proxy_password
 
-      # Set to true to test message sending; this will not actually send 
-      # messages but will collect sent messages in a testable collection. 
+      # Set to true to test message sending; this will not actually send
+      # messages but will collect sent messages in a testable collection.
       # (off by default)
       attr_accessor :test_mode
     end
     self.debug_mode = false
     self.secure_mode = false
     self.test_mode = false
-    
+
     # Creates a new API instance using the specified +auth options+.
-    # +auth_options+ is a hash containing either a :session_id or 
+    # +auth_options+ is a hash containing either a :session_id or
     # :username, :password and :api_key.
     #
-    # Some API calls (authenticate, ping etc.) do not require any 
+    # Some API calls (authenticate, ping etc.) do not require any
     # +auth_options+. +auth_options+ can be updated using the accessor methods.
     def initialize(auth_options={})
       @auth_options = auth_options
     end
-    
+
     # Authenticates using the specified credentials. Returns
     # a session_id if successful which can be used in subsequent
     # API calls.
@@ -57,18 +57,23 @@ module Clickatell
       )
       parse_response(response)['OK']
     end
-    
+
     # Pings the service with the specified session_id to keep the
     # session alive.
     def ping(session_id)
       execute_command('ping', 'http', :session_id => session_id)
     end
-    
+
     # Sends a message +message_text+ to +recipient+. Recipient
     # number should have an international dialing prefix and
     # no leading zeros (unless you have set a default prefix
-    # in your clickatell account centre). Messages over 153 characters
-    # are split into multiple messages.
+    # in your clickatell account centre).
+    #
+    # Messages over 160 characters are split into multiple messages automatically,
+    # and the :concact option will be set, overwriting any manual value of this option.
+    #
+    # You normally wouldn't need to set :concat manually and can rely on the automatica
+    # splitting behaviour.
     #
     # Additional options:
     #    :from - the from number/name
@@ -87,7 +92,7 @@ module Clickatell
       recipient = recipient.join(",")if recipient.is_a?(Array)
       response = execute_command('sendmsg', 'http',
         {:to => recipient, :text => message_text}.merge(valid_options)
-      ) 
+      )
       response = parse_response(response)
       response.is_a?(Array) ? response.map { |r| r['ID'] } : response['ID']
     end
@@ -100,7 +105,7 @@ module Clickatell
       )
       parse_response(response)['ID']
     end
-    
+
     # Returns the status of a message. Use message ID returned
     # from original send_message call.
     def message_status(message_id)
@@ -112,31 +117,31 @@ module Clickatell
       response = execute_command('getmsgcharge', 'http', :apimsgid => message_id)
       parse_response(response)['charge'].to_f
     end
-    
+
     # Returns the number of credits remaining as a float.
     def account_balance
       response = execute_command('getbalance', 'http')
       parse_response(response)['Credit'].to_f
     end
-    
+
     def sms_requests
       @sms_requests ||= []
     end
 
-    protected      
+    protected
       def execute_command(command_name, service, parameters={}) #:nodoc:
         executor = CommandExecutor.new(auth_hash, self.class.secure_mode, self.class.debug_mode, self.class.test_mode)
         result = executor.execute(command_name, service, parameters)
-        
+
         (sms_requests << executor.sms_requests).flatten! if self.class.test_mode
-        
+
         result
       end
 
       def parse_response(raw_response) #:nodoc:
         Clickatell::Response.parse(raw_response)
       end
-      
+
       def auth_hash #:nodoc:
         if @auth_options[:session_id]
           { :session_id => @auth_options[:session_id] }
@@ -148,7 +153,7 @@ module Clickatell
           {}
         end
       end
-    
+
   end
 end
 
@@ -156,7 +161,7 @@ end
     api/command_executor
     api/error
     api/message_status
-    
+
 ).each do |lib|
     require File.join(File.dirname(__FILE__), lib)
 end
